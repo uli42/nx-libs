1a3bd9f4811a871f1d47e0d43ccf7ed0dc30118b
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2017-11-07 16:49:31 +0100

    mi: Backport latest devPrivates ABI from X.org.

--- nx-X11/programs/Xserver/mi/mi.h
+++ nx-X11/programs/Xserver/mi/mi.h
@@ -55,6 +55,8 @@ SOFTWARE.
 #include <X11/fonts/font.h>
 #include "input.h"
 #include "cursor.h"
+#include "privates.h"
+#include "colormap.h"
 
 #define MiBits	CARD32
 
@@ -455,7 +457,7 @@ extern Bool miScreenInit(
     VisualPtr /*visuals*/
 );
 
-extern int miAllocateGCPrivateIndex(
+extern DevPrivateKeyRec miAllocateGCPrivateKeyRec(
     void
 );
 
