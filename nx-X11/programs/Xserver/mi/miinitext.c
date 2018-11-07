cab4cdb5095ddb6e79a6a33eb372a8e33909bd4a
Ulrich Sibiller <uli42@gmx.de>
2018-11-07 18:25:25 +0100

    mi: re-activate Xext dependent code
    Had been delayed until Xext update

--- nx-X11/programs/Xserver/mi/miinitext.c
+++ nx-X11/programs/Xserver/mi/miinitext.c
@@ -218,10 +218,7 @@ extern void RecordExtensionInit(void);
 extern void DbeExtensionInit(void);
 #endif
 #ifdef XCSECURITY
-#if 0
-/* FIXME: need to update Xext first*/
 extern void SecurityExtensionSetup(void);
-#endif
 extern void SecurityExtensionInit(void);
 #endif
 #ifdef XF86BIGFONT
@@ -382,10 +379,7 @@ InitExtensions(argc, argv)
     char	*argv[];
 {
 #ifdef XCSECURITY
-#if 0
-    /* FIXME: need to update Xext first */
     SecurityExtensionSetup();
-#endif
 #endif
 #ifdef PANORAMIX
 # if !defined(PRINT_ONLY_SERVER) && !defined(NO_PANORAMIX)
