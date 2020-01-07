6d6a7b033a67ccdbf56f2bd3ab023497afe4e076
Ulrich Sibiller <uli42@gmx.de>
2020-01-07 21:27:04 +0100

    nxagent: Free atom map on termination

--- nx-X11/programs/Xserver/hw/nxagent/NXdispatch.c
+++ nx-X11/programs/Xserver/hw/nxagent/NXdispatch.c
@@ -584,8 +584,6 @@ Reply   Total	Cached	Bits In			Bits Out		Bits/Reply	  Ratio
     nxagentFreeFontData();
 #endif /* NXAGENT_SERVER */
 
-    nxagentFreeAtomMap();
-
     KillAllClients();
     free(clientReady);
     dispatchException &= ~DE_RESET;
