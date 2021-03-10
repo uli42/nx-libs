e042f59c5eeb72e5e00a18940f1b27d0611084da
Ulrich Sibiller <uli42@gmx.de>
2021-03-10 23:04:01 +0100

    Compext.c: fix misleading error message
    An X reply contains a type which is X_Reply or X_Error. This is not an
    opcode which is when installing the handler.

--- nx-X11/programs/Xserver/hw/nxagent/compext/Compext.c
+++ nx-X11/programs/Xserver/hw/nxagent/compext/Compext.c
@@ -3463,7 +3463,7 @@ static Bool _NXCollectImageHandler(Display *dpy, xReply *rep, char *buf,
           ((int)(state -> sequence) % 65536))
   {
     #ifdef TEST_IMAGE
-    fprintf(stderr, "******_NXCollectImageHandler: Unmatched sequence [%d] for opcode [%d] "
+    fprintf(stderr, "******_NXCollectImageHandler: Unmatched sequence [%d] of type [%d] "
                 "with length [%d].\n", rep -> generic.sequenceNumber, rep -> generic.type,
                     (int) rep -> generic.length << 2);
     #endif
@@ -3861,9 +3861,10 @@ static Bool _NXCollectPropertyHandler(Display *dpy, xReply *rep, char *buf,
           ((int)(state -> sequence) % 65536))
   {
     #ifdef TEST_PROPERTY
-    fprintf(stderr, "******_NXCollectPropertyHandler: Unmatched sequence [%d] for opcode [%d] "
-                "with length [%d].\n", rep -> generic.sequenceNumber, rep -> generic.type,
+    fprintf(stderr, "******_NXCollectPropertyHandler: Unmatched sequence [%d] of type [%d] "
+            "with length [%d].\n", rep -> generic.sequenceNumber, rep -> generic.type,
                     (int) rep -> generic.length << 2);
+
     #endif
 
     return False;
@@ -4234,7 +4235,7 @@ static Bool _NXCollectGrabPointerHandler(Display *dpy, xReply *rep, char *buf,
           ((int)(state -> sequence) % 65536))
   {
     #ifdef TEST_POINTER
-    fprintf(stderr, "******_NXCollectGrabPointerHandler: Unmatched sequence [%d] for opcode [%d] "
+    fprintf(stderr, "******_NXCollectGrabPointerHandler: Unmatched sequence [%d] of type [%d] "
                 "with length [%d].\n", rep -> generic.sequenceNumber, rep -> generic.type,
                     (int) rep -> generic.length << 2);
     #endif
@@ -4526,7 +4527,7 @@ static Bool _NXCollectInputFocusHandler(Display *dpy, xReply *rep, char *buf,
           ((int)(state -> sequence) % 65536))
   {
     #ifdef TEST_INPUT
-    fprintf(stderr, "******_NXCollectInputFocusHandler: Unmatched sequence [%d] for opcode [%d] "
+    fprintf(stderr, "******_NXCollectInputFocusHandler: Unmatched sequence [%d] of type [%d] "
                 "with length [%d].\n", rep -> generic.sequenceNumber, rep -> generic.type,
                     (int) rep -> generic.length << 2);
     #endif
