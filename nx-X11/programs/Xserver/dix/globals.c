a0a9cb7b4c8d8ce030ce125e37bc17f6ef551019
Ulrich Sibiller <uli42@gmx.de>
2024-03-24 23:29:37 +0100

    fixup! Lift dix to Xorg 1.5.0 state

--- nx-X11/programs/Xserver/dix/globals.c
+++ nx-X11/programs/Xserver/dix/globals.c
@@ -27,13 +27,13 @@ Copyright 1987 by Digital Equipment Corporation, Maynard, Massachusetts.
 
                         All Rights Reserved
 
-Permission to use, copy, modify, and distribute this software and its
-documentation for any purpose and without fee is hereby granted,
+Permission to use, copy, modify, and distribute this software and its 
+documentation for any purpose and without fee is hereby granted, 
 provided that the above copyright notice appear in all copies and that
-both that copyright notice and this permission notice appear in
+both that copyright notice and this permission notice appear in 
 supporting documentation, and that the name of Digital not be
 used in advertising or publicity pertaining to distribution of the
-software without specific, written prior permission.
+software without specific, written prior permission.  
 
 DIGITAL DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
 ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
@@ -149,13 +149,9 @@ int defaultColorVisualClass = -1;
 int monitorResolution = 0;
 
 char *display;
-int displayfd = -1;
-Bool explicit_display = FALSE;
 
 CARD32 TimeOutValue = DEFAULT_TIMEOUT * MILLI_PER_SECOND;
 int	argcGlobal;
 char	**argvGlobal;
 
 DDXPointRec dixScreenOrigins[MAXSCREENS];
-
-char *ConnectionInfo;
