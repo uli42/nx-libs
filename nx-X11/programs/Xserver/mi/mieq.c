f7771ed7c58001b93c53a8d4553aa3645063d2ee
Ulrich Sibiller <uli42@gmx.de>
2024-04-27 21:10:32 +0200

    mi: Hush mieqSetHandler for no-op changes
      commit 801977e8252f87f9ab156c7e02ad00fb1d70f8e5
      Author: Adam Jackson <ajax@redhat.com>
      Date:   Mon Aug 27 16:34:20 2018 -0400
    
        mi: Hush mieqSetHandler for no-op changes
    
        If we set the handler to the thing it's already set to, that's fine.
    
        Signed-off-by: Adam Jackson <ajax@redhat.com>

--- nx-X11/programs/Xserver/mi/mieq.c
+++ nx-X11/programs/Xserver/mi/mieq.c
@@ -191,7 +191,7 @@ mieqSwitchScreen(ScreenPtr pScreen, Bool fromDIX)
 void
 mieqSetHandler(int event, mieqHandler handler)
 {
-    if (handler && miEventQueue.handlers[event])
+    if (handler && miEventQueue.handlers[event] != handler)
         ErrorF("mieq: warning: overriding existing handler %p with %p for "
                "event %d\n", miEventQueue.handlers[event], handler, event);
 
