2e8755b5c6995ec51b60c7221cca646aeea4ef54
Ulrich Sibiller <uli42@gmx.de>
2024-04-27 14:35:17 +0200

    dix/getevents.c: add xorg-xserver 1.5.3 fixes

--- nx-X11/programs/Xserver/dix/getevents.c
+++ nx-X11/programs/Xserver/dix/getevents.c
@@ -410,9 +410,6 @@ GetKeyboardValuatorEvents(xEvent *events, DeviceIntPtr pDev, int type,
     KeySym sym;
     deviceKeyButtonPointer *kbp = NULL;
 
-    sym = map[(key_code - pDev->key->curKeySyms.minKeyCode)
-              * pDev->key->curKeySyms.mapWidth];
-
     if (!events)
         return 0;
 
@@ -426,6 +423,9 @@ GetKeyboardValuatorEvents(xEvent *events, DeviceIntPtr pDev, int type,
 
     if (key_code < 8 || key_code > 255)
         return 0;
+
+    sym = map[(key_code - pDev->key->curKeySyms.minKeyCode)
+              * pDev->key->curKeySyms.mapWidth];
 
     if (pDev->coreEvents)
         numEvents = 2;
