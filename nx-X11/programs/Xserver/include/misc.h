9d7b76879b905ee67a23e143bffba500efe12acd
Ulrich Sibiller <uli42@gmx.de>
2024-05-20 19:02:28 +0200

    fixup! dix: lift to xorg-xserver 1.9.5 state

--- nx-X11/programs/Xserver/include/misc.h
+++ nx-X11/programs/Xserver/include/misc.h
@@ -211,6 +211,28 @@ pad_to_int32(const int bytes) {
     return (((bytes) + 3) & ~3);
 }
 
+/**
+ * Compare the two version numbers comprising of major.minor.
+ *
+ * @return A value less than 0 if a is less than b, 0 if a is equal to b,
+ * or a value greater than 0
+ */
+static inline int
+version_compare(uint32_t a_major, uint32_t a_minor,
+                uint32_t b_major, uint32_t b_minor)
+{
+    if (a_major > b_major)
+        return 1;
+    if (a_major < b_major)
+        return -1;
+    if (a_minor > b_minor)
+        return 1;
+    if (a_minor < b_minor)
+        return -1;
+
+    return 0;
+}
+
 extern char**
 xstrtokenize(const char *str, const char* separators);
 
