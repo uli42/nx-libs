d6dae5c01241514cfd8d5c0d8368b8379f07aac4
Ulrich Sibiller <uli42@gmx.de>
2024-06-08 15:36:18 +0200

    Xi: silence compiler warnings (set but not used)
    Backport of this xorg-xserver commit
    
      commit 484cef5b29ef82402a15e155b3b8505b1e4a6830
      Author: Peter Hutterer <peter.hutterer@who-t.net>
      Date:   Tue Aug 2 09:21:00 2011 +1000
    
        Xi: silence compiler warnings (set but not used)
    
        exevents.c: In function 'UpdateDeviceState':
        exevents.c:719:9: warning: variable 'bit' set but not used
        [-Wunused-but-set-variable]
    
        exevents.c: In function 'ProcessOtherEvent':
        exevents.c:889:22: warning: variable 'v' set but not used
        [-Wunused-but-set-variable]
        exevents.c:888:17: warning: variable 'k' set but not used
        [-Wunused-but-set-variable]
    
        Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
        Reviewed-by: Daniel Stone <daniel@fooishbar.org>

--- nx-X11/programs/Xserver/Xi/exevents.c
+++ nx-X11/programs/Xserver/Xi/exevents.c
@@ -733,7 +733,6 @@ UpdateDeviceState(DeviceIntPtr device, DeviceEvent* event)
 {
     int i;
     int key = 0,
-        bit = 0,
         last_valuator;
 
     KeyClassPtr k       = NULL;
@@ -767,7 +766,6 @@ UpdateDeviceState(DeviceIntPtr device, DeviceEvent* event)
     b = device->button;
 
     key = event->detail.key;
-    bit = 1 << (key & 7);
 
     /* Update device axis */
     /* Check valuators first */
@@ -931,8 +929,6 @@ ProcessOtherEvent(InternalEvent *ev, DeviceIntPtr device)
     Bool deactivateDeviceGrab = FALSE;
     int key = 0, rootX, rootY;
     ButtonClassPtr b;
-    KeyClassPtr k;
-    ValuatorClassPtr v;
     int ret = 0;
     int state, i;
     DeviceIntPtr mouse = NULL, kbd = NULL;
@@ -996,9 +992,7 @@ ProcessOtherEvent(InternalEvent *ev, DeviceIntPtr device)
     if (ret == DONT_PROCESS)
         return;
 
-    v = device->valuator;
     b = device->button;
-    k = device->key;
 
     if (IsMaster(device) || !device->u.master)
         CheckMotion(event, device);
