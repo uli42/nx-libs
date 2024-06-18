26153d9018c4a9ceea2ca6b8494f4cc250d67c28
Ulrich Sibiller <uli42@gmx.de>
2024-06-18 23:01:34 +0200

    mi/misprite: use memory management provided by dixRegisterPrivateKey
    Backport of this xorg-xserver commit:
    
      commit a8146f6becc44bf9ad611d33bded17df07e6af21
      Author: Erkki Seppälä <erkki.seppala@vincit.fi>
      Date:   Wed Mar 9 17:29:14 2011 +0200
    
        mi/misprite: use memory management provided by dixRegisterPrivateKey
    
        The record allocated by miSpriteDeviceCursorInitialize was not being
        released.
    
        This patch makes misprite use dixRegisterPrivateKey with the record
        size argument, which handles the memory management
        issues. miSpriteDeviceCursorInitialize is restructured to initialize
        pCursorInfo only if miDCDeviceInitialize succeeds. The record itself
        is zeroed on cleanup to ensure that the assumptions in the code still
        hold.
    
        Reviewed-by: Rami Ylimäki <rami.ylimaki@vincit.fi>
        Signed-off-by: Erkki Seppälä <erkki.seppala@vincit.fi>
        Reviewed-by: Daniel Stone <daniel@fooishbar.org>
        Signed-off-by: Keith Packard <keithp@keithp.com>

--- nx-X11/programs/Xserver/mi/misprite.c
+++ nx-X11/programs/Xserver/mi/misprite.c
@@ -296,7 +296,8 @@ miSpriteInitialize (ScreenPtr               pScreen,
     if (!dixRegisterPrivateKey(&miSpriteScreenKeyRec, PRIVATE_SCREEN, 0))
 	return FALSE;
 
-    if (!dixRegisterPrivateKey(&miSpriteDevPrivatesKeyRec, PRIVATE_DEVICE, 0))
+    /* backport a8146f6becc44bf9ad611d33bded17df07e6af21 */
+    if (!dixRegisterPrivateKey(&miSpriteDevPrivatesKeyRec, PRIVATE_DEVICE, sizeof(miCursorInfoRec)))
 	return FALSE;
 
     pScreenPriv = malloc(sizeof (miSpriteScreenRec));
@@ -835,38 +836,38 @@ miSpriteMoveCursor (DeviceIntPtr pDev, ScreenPtr pScreen, int x, int y)
 static Bool
 miSpriteDeviceCursorInitialize(DeviceIntPtr pDev, ScreenPtr pScreen)
 {
-    miCursorInfoPtr pCursorInfo;
-    int ret = FALSE;
+    /* backport a8146f6becc44bf9ad611d33bded17df07e6af21 */
+    int ret = miDCDeviceInitialize(pDev, pScreen);
 
-    pCursorInfo = malloc(sizeof(miCursorInfoRec));
-    if (!pCursorInfo)
-        return FALSE;
-
-    pCursorInfo->pCursor = NULL;
-    pCursorInfo->x = 0;
-    pCursorInfo->y = 0;
-    pCursorInfo->isUp = FALSE;
-    pCursorInfo->shouldBeUp = FALSE;
-    pCursorInfo->pCacheWin = NullWindow;
-    pCursorInfo->isInCacheWin = FALSE;
-    pCursorInfo->checkPixels = TRUE;
-    pCursorInfo->pScreen = FALSE;
-
-    ret = miDCDeviceInitialize(pDev, pScreen);
-    if (!ret)
+    if (ret)
     {
-        free(pCursorInfo);
-        pCursorInfo = NULL;
+        miCursorInfoPtr pCursorInfo;
+        pCursorInfo = dixLookupPrivate(&pDev->devPrivates, miSpriteDevPrivatesKey);
+        pCursorInfo->pCursor = NULL;
+        pCursorInfo->x = 0;
+        pCursorInfo->y = 0;
+        pCursorInfo->isUp = FALSE;
+        pCursorInfo->shouldBeUp = FALSE;
+        pCursorInfo->pCacheWin = NullWindow;
+        pCursorInfo->isInCacheWin = FALSE;
+        pCursorInfo->checkPixels = TRUE;
+        pCursorInfo->pScreen = FALSE;
     }
-    dixSetPrivate(&pDev->devPrivates, miSpriteDevPrivatesKey, pCursorInfo);
+
     return ret;
 }
 
 static void
 miSpriteDeviceCursorCleanup(DeviceIntPtr pDev, ScreenPtr pScreen)
 {
+    /* backport a8146f6becc44bf9ad611d33bded17df07e6af21 */
+    miCursorInfoPtr pCursorInfo = dixLookupPrivate(&pDev->devPrivates, miSpriteDevPrivatesKey);
+
     if (DevHasCursor(pDev))
         miDCDeviceCleanup(pDev, pScreen);
+
+    /* backport a8146f6becc44bf9ad611d33bded17df07e6af21 */
+    memset(pCursorInfo, 0, sizeof(miCursorInfoRec));
 }
 
 /*
